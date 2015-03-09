b1d3833d57b6354c919a6fe4aeb4d0932074903e
daotrancong <daotrancong@sohagame.vn>
2015-03-09 23:22:42 +0700

    ban chinh

--- local/app/views/chung_loais/index.blade.php
+++ local/app/views/chung_loais/index.blade.php
@@ -13,31 +13,25 @@ Chủng loại sản phẩm
 
 <p>{{ link_to_route('chung_loais.create', 'Thêm mới') }}</p>
 
-@if ($chung_loais->count())
+
 	<table class="table table-striped table-bordered">
 		<thead>
-			<tr>    
-                                <th>ID</th>
+			<tr>
+                <th>ID</th>
 				<th>Tên chủng loại</th>
 			</tr>
 		</thead>
 
 		<tbody>
-			@foreach ($chung_loais as $chung_loai)
-				<tr>     <td>{{{ $chung_loai->id }}}</td>   
-					<td>{{{ $chung_loai->tenchungloai }}}</td>
-                    <td>{{ link_to_route('chung_loais.edit', 'Edit', array($chung_loai->id), array('class' => 'btn btn-info')) }}</td>
+				<tr>
+					<td></td>
+					<td></td>
+                    <td></td>
                     <td>
-                        {{ Form::open(array('method' => 'DELETE', 'route' => array('chung_loais.destroy', $chung_loai->id))) }}
-                            {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
-                        {{ Form::close() }}
+
                     </td>
 				</tr>
-			@endforeach
 		</tbody>
 	</table>
-@else
-	Chưa có chủng loại nào được thêm
-@endif
 
 @stop
